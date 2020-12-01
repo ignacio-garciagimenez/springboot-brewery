8c3c4413550e22f4eba77120de21e311dc6eff07
ignaciogarciagim <ignacio.easyorder@gmail.com>
2020-12-01 14:56:16 -0300

    First commit

---
+++ src/test/java/com/springboot/springbootbrewery/SpringbootBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
