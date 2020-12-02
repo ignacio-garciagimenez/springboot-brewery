150fdb364f56cae25903d37c8e78f84639d508e0
ignacio-garciagimenez <ignacio.easyorder@gmail.com>
2020-12-01 21:00:06 -0300

    Web Api structure ready

---
+++ src/main/java/com/springboot/springbootbrewery/web/controller/BeerController.java
@@ -0,0 +1,37 @@
+package com.springboot.springbootbrewery.web.controller;
+
+import com.springboot.springbootbrewery.web.model.BeerDto;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+
+    @GetMapping({"{beerId}"})
+    public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
+
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity createBeer(@RequestBody BeerDto beerDto) {
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping({"{beerId}"})
+    public ResponseEntity updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/springboot/springbootbrewery/web/model/BeerDto.java
@@ -0,0 +1,29 @@
+package com.springboot.springbootbrewery.web.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
